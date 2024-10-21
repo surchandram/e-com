558b6f4cef9ea0c2eba04d682d8953429bada5b8
MOIRANGTHEM SURCHANDRA SINGH <surrrchandra@gmail.com>
2024-10-21 20:41:48 +0530

    2nd

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_KEY, { expiresIn: "3d" });
+  return jwt.sign({ id }, process.env.JWT_KEY, { expiresIn: "30d" });
 };
 
 export default generateToken;
